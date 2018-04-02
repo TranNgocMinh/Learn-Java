814ec6264e30e43f66ca0096b379c0eae8c7929f
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-04-02 21:02:27 +0700

    Create showMessageDialog.java

---
+++ showMessageDialog.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class MyMainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JOptionPane.showMessageDialog(null, "Hello World!", "Greetings", JOptionPane.ERROR_MESSAGE);
+    JOptionPane.showMessageDialog(null, "Hello World!", "Greetings",JOptionPane.INFORMATION_MESSAGE);
+
+	}
+
+}
