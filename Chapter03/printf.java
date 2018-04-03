0860b066b311ffada16409ff19a85c3017336b60
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-04-03 08:23:03 +0700

    Create printf.java

---
+++ Chapter03/printf.java
@@ -0,0 +1,13 @@
+public class MyMainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+		double PI = (double) 3.14f;	
+		System.out.println("PI = "+ PI);//3.140000104904175
+		System.out.printf("PI = %.2f", PI);//3.14
+
+		
+	}
+
+}
