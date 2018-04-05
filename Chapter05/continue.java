d0234855edb57c37c3f5b143f09203d8b257eca1
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-04-05 15:10:37 +0700

    Update continue.java

--- Chapter05/continue.java
+++ Chapter05/continue.java
@@ -1,8 +1,9 @@
 String numseq = "";
-		for (int i = 1; i <= 10; i++) {
-			if (i == 5) {
-				continue;
-			}
-			numseq += " " + i;
-		}
-System.out.println(numseq);
+for (int i = 1; i <= 10; i++) {
+	if (i == 5) {
+		continue;
+	}
+	numseq += " " + i;
+}
+System.out.println(numseq);// 1 2 3 4 6 7 8 9 10
+
