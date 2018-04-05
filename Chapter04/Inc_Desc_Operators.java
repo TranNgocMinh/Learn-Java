a41a62940e22d824bfebe8361ec65c5517357f75
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-04-05 14:42:56 +0700

    Create Inc_Desc_Operators.java

---
+++ Chapter04/Inc_Desc_Operators.java
@@ -0,0 +1,11 @@
+public class MyMainClass {
+
+	public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+		int x, y;
+		x = 4;
+		System.out.print (x++);// 4
+		x = 4;
+		System.out.print (++x); // 5
+	}
+}
