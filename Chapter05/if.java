d076aed158fefb54698d3f8cebd68ed5be02778f
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-04-05 14:50:38 +0700

    Create if.java

---
+++ Chapter05/if.java
@@ -0,0 +1,10 @@
+public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+		int age = 16;
+		String message ="";
+		if (age < 18) {     
+			message = "You are under 18 years of age!";
+		}
+		System.out.print(message);
+	}
+	
