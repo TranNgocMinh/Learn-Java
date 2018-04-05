50c489113a9f551470403592d1903e580a515ccd
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-04-05 15:06:54 +0700

    Update do_while.java

--- Chapter05/do_while.java
+++ Chapter05/do_while.java
@@ -1,11 +1,11 @@
 // using do loop
 int i = 11;
-		String numseq = "";
-		while (i < 10)
-		{
-			numseq += " " + i;
-			i = i + 1;
-		}
+String numseq = "";
+while (i < 10)
+{
+	numseq += " " + i;
+	i = i + 1;
+}
 System.out.println("The number is " + numseq);// The number is
 // using while loop
 int i = 11;
