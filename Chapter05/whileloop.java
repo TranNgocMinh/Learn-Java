d3ecbd0600913c78110969addc310d648040fcca
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-04-05 14:59:26 +0700

    Create whileloop.java

---
+++ Chapter05/whileloop.java
@@ -0,0 +1,11 @@
+public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+		int i = 0;
+		String numseq = "";
+		while (i < 10)
+		{
+		numseq += " " + i;
+		i = i + 1; // or i++;
+		}
+		System.out.println("The number sequence is "+ numseq);
+	}
