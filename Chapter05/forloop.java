18eb7ea4635b14fda40100cdeb475ddd9c04ea97
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-04-05 15:00:13 +0700

    Create forloop.java

---
+++ Chapter05/forloop.java
@@ -0,0 +1,10 @@
+public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+		int i;
+		String numseq = "";
+		for (i = 1; i <= 10; i++)
+		{
+		numseq += " " + i;
+		}
+		System.out.println("The number sequence is "+ numseq);
+	}
