5c55f67fa108e2b8c13ea31bffc734be20162b8f
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-04-05 14:52:19 +0700

    Create if_else.java

---
+++ Chapter05/if_else.java
@@ -0,0 +1,13 @@
+public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+		int age = 19;
+		String message ="";
+		if (age < 18) {      
+		 	message = "You are under 18 years of age!";
+		}
+		else { 
+		      message = "Welcome to our website!";
+		}
+		System.out.print(message);
+
+	}
