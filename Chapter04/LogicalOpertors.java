e09ebf6e4c8fdbf9b6c9f5e684a14593d1e36fde
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-04-05 14:39:13 +0700

    Create LogicalOpertors.java

---
+++ Chapter04/LogicalOpertors.java
@@ -0,0 +1,13 @@
+public class MyMainClass {
+
+	public static void main(String[] args)  {
+		// TODO Auto-generated method stub
+		int num1 = 3;
+		int num2 = 7;
+		System.out.println(num1 == 3 & num2 == 7);	//true
+		System.out.println(num1 == 2 && num2 == 7);//false
+		System.out.println(num1 == 3 | num2 == 11);//true
+		System.out.println(!(num1 == 5));//true
+		System.out.println(num1 == 2 && num2 == 7);//false
+	}
+}
