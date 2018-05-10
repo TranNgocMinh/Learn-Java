5f1489b807a4605a77dd05484df2ccbc8da5f753
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-05-10 09:20:39 +0700

    Create instanceof.java

---
+++ Chapter06/instanceof.java
@@ -0,0 +1,11 @@
+Person obj = new Student();
+			
+if(obj instanceof Person)
+	System.out.println("I am a Person.");
+else
+	System.out.println("I am not a Person.");
+			
+if(obj instanceof Employee)
+	System.out.println("I am an Employee.");
+else
+	System.out.println("I am not an Employee.");
