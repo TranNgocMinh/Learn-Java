2f7fb585eebc6271258ce4aaa908d048c9e8f329
Ngoc Minh <tranngocminhcdn@gmail.com>
2018-05-10 09:33:16 +0700

    Update Array2D.java

--- Chapter07/Array2D.java
+++ Chapter07/Array2D.java
@@ -1,15 +1,15 @@
 public class MyMainClass {
 
 	public static void main(String[] args)  {
-		    float[][] arr2D = new float[7][5];
-        arr2D[0][0] = 50.5f;
-        arr2D[4][4] = 60.5f;
-        arr2D[3][1] = 10.3f;
-        for (int i = 0; i < 7; i++) { 
-	          for (int j = 0; j < 5; j++)
+		float[][] arr2D = new float[7][5];
+        	arr2D[0][0] = 50.5f;
+       		arr2D[4][4] = 60.5f;
+        	arr2D[3][1] = 10.3f;
+        	for (int i = 0; i < 7; i++) { 
+	        	for (int j = 0; j < 5; j++)
 		            System.out.print(array2d[i][j] + " ");
-	          System.out.println();			   
-        }			
+	        System.out.println();			   
+        	}			
 	}
 }
 //Kết quả:
